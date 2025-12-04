5322674e9dfdd3b5b8bed7023a7c1708b801e421
Nils Elveros <nils.elveros@tromb.com>
2025-12-04 14:12:36 +0100

    fix: spotless

--- src/main/java/se/fk/github/rimfrost/operativt/uppgiftslager/presentation/kafka/util/OperativtUppgiftslagerStatusControlDeserializer.java
+++ src/main/java/se/fk/github/rimfrost/operativt/uppgiftslager/presentation/kafka/util/OperativtUppgiftslagerStatusControlDeserializer.java
@@ -3,9 +3,10 @@ package se.fk.github.rimfrost.operativt.uppgiftslager.presentation.kafka.util;
 import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
 import se.fk.rimfrost.OperativtUppgiftslagerStatusMessage;
 
-public class OperativtUppgiftslagerStatusControlDeserializer extends ObjectMapperDeserializer<OperativtUppgiftslagerStatusMessage> {
-    public OperativtUppgiftslagerStatusControlDeserializer()
-    {
-        super(OperativtUppgiftslagerStatusMessage.class);
-    }
+public class OperativtUppgiftslagerStatusControlDeserializer extends ObjectMapperDeserializer<OperativtUppgiftslagerStatusMessage>
+{
+   public OperativtUppgiftslagerStatusControlDeserializer()
+   {
+      super(OperativtUppgiftslagerStatusMessage.class);
+   }
 }
