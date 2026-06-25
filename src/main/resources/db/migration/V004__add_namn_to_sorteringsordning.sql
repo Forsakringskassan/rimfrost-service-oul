846780360727b1c2072859e484147d74abe0338f
Lars Persson <254442132+larpersso@users.noreply.github.com>
2026-06-25 09:11:06 +0200

    fix: Fix null issue with sorteringsordning namn migration

--- src/main/resources/db/migration/V004__add_namn_to_sorteringsordning.sql
+++ src/main/resources/db/migration/V004__add_namn_to_sorteringsordning.sql
@@ -1,2 +1,3 @@
-ALTER TABLE sorteringsordning ADD COLUMN namn VARCHAR NOT NULL;
+ALTER TABLE sorteringsordning ADD COLUMN namn VARCHAR NOT NULL DEFAULT '';
 UPDATE sorteringsordning SET namn = id::varchar WHERE namn = '';
+ALTER TABLE sorteringsordning ALTER COLUMN namn DROP DEFAULT;
